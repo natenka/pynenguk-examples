43cdd93cfe0c83cabf05a98de80c96f970bd4f5d
natenka <natasha.samoylenko@gmail.com>
2023-06-21 19:34:30 +0000

    UPD 04

---
+++ examples/04_data_structures/basics_04_list.py
@@ -0,0 +1,9 @@
+
+items = [1, 2, 3]
+
+ip1 = "10.1.1.1"
+ip2 = "10.2.2.2"
+ip3 = "10.3.3.3"
+
+ip_list = [ip1, ip2, ip3]
+print(ip_list)
---
+++ examples/04_data_structures/basics_05_list_methods.py
@@ -0,0 +1,14 @@
+
+ip_list = []
+ip_list.append("10.1.1.1")
+ip_list.append("10.2.2.2")
+print(ip_list)
+
+# ip_list = ["10.1.1.1", "10.2.2.2"]
+
+items = []
+for num in [1, 2, 3]:
+    num_str = str(num)
+    items.append(num_str)
+
+print(items)
