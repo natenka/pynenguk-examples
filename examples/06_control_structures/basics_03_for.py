c67f2fe4bc2d552bfac205fda69cf88a08172fa0
natenka <natasha.samoylenko@gmail.com>
2023-07-06 08:27:47 +0000

    UPD 06_control_structures

--- examples/06_control_structures/basics_03_for.py
+++ examples/06_control_structures/basics_03_for.py
@@ -1,1 +1,12 @@
+cmd = "switchport trunk allowed vlan 10,20,30,40,50"
+vlans_list = cmd.split()[-1].split(",")
+# vlans_list = ['10', '20', '30', '40', '50']
+print(vlans_list)
+
+vlans = []
+for vl in vlans_list:
+    vl_int = int(vl)
+    vlans.append(vl_int)
+
+print(vlans)
 
