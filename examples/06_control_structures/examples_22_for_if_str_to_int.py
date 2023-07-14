45cc71f2e9bbe2532ccfc36125aa0e17a008c25f
natenka <natasha.samoylenko@gmail.com>
2023-07-14 07:43:58 +0000

    UPD 06_control_structures

--- examples/06_control_structures/examples_22_for_if_str_to_int.py
+++ examples/06_control_structures/examples_22_for_if_str_to_int.py
@@ -1,3 +1,10 @@
 
-vlan_list = ["1", "2", "trunk", "3", "4", "mode", "5"]
+vlan_list = ["1", "2", "trunk", "3", "4", "mode", " 5"]
 
+vlans_int = []
+for vl in vlan_list:
+    vl = vl.strip()
+    if vl.isdigit():
+        vlans_int.append(int(vl))
+
+print(vlans_int)
