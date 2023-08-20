e69ea0c0079c549ea18fda151c282caf2d7d4bbf
natenka <natasha.samoylenko@gmail.com>
2023-08-20 10:48:41 +0000

    UPD 09 examples

--- examples/09_functions/tips_and_tricks_01_arg_unpack_pos.py
+++ examples/09_functions/tips_and_tricks_01_arg_unpack_pos.py
@@ -4,8 +4,8 @@ def config_interface(intf_name, ip_address, mask):
     interface = f"interface {intf_name}"
     no_shut = "no shutdown"
     ip_addr = f"ip address {ip_address} {mask}"
-    result = [interface, no_shut, ip_addr]
-    return result
+    commands = [interface, no_shut, ip_addr]
+    return commands
 
 
 # print(config_interface("Fa0/1", "10.0.1.1", "255.255.255.0"))
